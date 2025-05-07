b22c49000f392f5fab42254854cbeb7d2f05465e
Przemek Szypowicz <p.code@szypowi.cz>
2025-05-07 12:23:49 +0200

    use subscription

--- dot-config/fish/functions/spn-login.fish
+++ dot-config/fish/functions/spn-login.fish
@@ -9,6 +9,7 @@ function spn-login --description "Login to Azure using a service principal" --de
 
   op-spn --spn "$_flag_spn[1]" --export-env
   az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+  az account set --subscription $ARM_SUBSCRIPTION_ID
 
   if not set -ql _flag_export_env
     set --unexport ARM_SUBSCRIPTION_ID
