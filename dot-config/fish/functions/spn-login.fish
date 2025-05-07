9da3495b9c6ae439fd776a2d48529b052391f110
Przemysław Szypowicz <2733699+pszypowicz@users.noreply.github.com>
2025-05-07 13:45:12 +0200

    supress output from az login

--- dot-config/fish/functions/spn-login.fish
+++ dot-config/fish/functions/spn-login.fish
@@ -8,7 +8,7 @@ function spn-login --description "Login to Azure using a service principal" --de
   end
 
   op-spn --spn "$_flag_spn[1]" --export-env
-  az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+  az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID --only-show-errors --output none
   az account set --subscription $ARM_SUBSCRIPTION_ID
 
   if not set -ql _flag_export_env
