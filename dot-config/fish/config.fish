77c6e99a66f3a757b3e21dba07ae774e4dc6605e
Przemek Szypowicz <p.code@szypowi.cz>
2025-05-20 11:18:02 +0200

    snapshot

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,4 +1,3 @@
-
 set fish_greeting
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -6,20 +5,19 @@ fish_add_path ~/bin
 
 # Set the default editor
 set -gx EDITOR vim
+set -gx FZF_DEFAULT_COMMAND 'fd --type f'
+set -gx FZF_DEFAULT_OPTS '--tmux bottom,80%,40%'
+set -g fish_key_bindings fish_vi_key_bindings
 
 if status is-interactive
-  starship init fish | source
-  fzf --fish | source
 
-  ssh-add --apple-use-keychain -q
+    starship init fish | source
+    fzf --fish | source
 
-  set -g fish_key_bindings fish_vi_key_bindings
+    ssh-add --apple-use-keychain -q
 
-  set -gx FZF_DEFAULT_COMMAND 'fd --type f'
-  set -gx FZF_DEFAULT_OPTS '--tmux bottom,80%,40%'
-
-  # Check if tmux is already running by checking the TMUX environment variable
-  set -q TMUX || start_tmux
+    # Check if tmux is already running by checking the TMUX environment variable
+    if not set -q TMUX
+        start_tmux
+    end
 end
-
-
