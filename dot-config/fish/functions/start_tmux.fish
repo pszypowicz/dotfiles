c130f1df0175928306b767e1b890a904c6944523
Przemek Szypowicz <p.code@szypowi.cz>
2025-05-27 12:20:59 +0200

    Do not reattach to vscode tmux sessions from terminal

--- dot-config/fish/functions/start_tmux.fish
+++ dot-config/fish/functions/start_tmux.fish
@@ -1,15 +1,18 @@
 function start_tmux
-  # Check if tmux is already running
-  # and if there are any unattached sessions
-  # Get the list of unattached sessions
+    # Check if tmux is already running
+    # and if there are any unattached sessions
 
-  set tmux_unattached_sessions (tmux list-sessions -F '#{session_name}' -f '#{==:#{session_attached},0}')
+    # Example for listing all unattached tmux sessions
+    # set tmux_unattached_sessions (tmux list-sessions -F '#{session_name}' -f '#{==:#{session_attached},0}')
 
-  # If there are unattached sessions, attach to the first one
-  if test -n "$tmux_unattached_sessions"
-    exec tmux attach -t "$tmux_unattached_sessions[1]"
-  else
-    # If no unattached sessions, start a new tmux session
-    exec tmux
-  end
+    # Get the list of unattached sessions where session name does not start with "vscode-"
+    set tmux_unattached_sessions (tmux list-sessions -F '#{session_name}' -f '#{&&:#{==:#{session_attached},0},#{!:#{m:vscode-*,#{session_name}}}}')
+
+    # If there are unattached sessions, attach to the first one
+    if test -n "$tmux_unattached_sessions"
+        exec tmux attach -t "$tmux_unattached_sessions[1]"
+    else
+        # If no unattached sessions, start a new tmux session
+        exec tmux
+    end
 end
