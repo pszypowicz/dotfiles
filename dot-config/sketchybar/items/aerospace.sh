c1592f5c5c947f265d0a0e64de518889286419ba
Przemysław Szypowicz <2733699+pszypowicz@users.noreply.github.com>
2025-07-15 12:10:09 +0200

    cleanup and rewrite aerospace

--- dot-config/sketchybar/items/aerospace.sh
+++ dot-config/sketchybar/items/aerospace.sh
@@ -5,25 +5,11 @@ set -e
 sketchybar --add event aerospace_focus_changed
 sketchybar --add event aerospace_workspace_change
 
-sketchybar \
-  --add item aerospace left \
-  --set aerospace \
-  drawing=off \
-  script="$PLUGIN_DIR/aerospace.sh" \
-  --subscribe aerospace aerospace_workspace_change \
-  --subscribe aerospace aerospace_focus_changed
-
-for sid in $(aerospace list-workspaces --all);
-do
-    sketchybar \
-      --add space space.$sid left \
-      --set space.$sid \
-      background.drawing=off \
-      icon.drawing=off \
-      label="$sid" \
-      label.highlight=off \
-      label.highlight_color=0xffFF0000 \
-      click_script="aerospace workspace $sid" \
-      script="exit 0" \
-      display=0
-done
+sketchybar --add alias "AeroSpace" left \
+  --set AeroSpace \
+  icon.drawing=off \
+  label.drawing=off \
+  padding_left=0 \
+  padding_right=0 \
+  --subscribe AeroSpace aerospace_workspace_change \
+  --subscribe AeroSpace aerospace_focus_changed
