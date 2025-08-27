4098efcfa2e5c68be61d35d1328cccc92e662d88
Przemek Szypowicz <p.code@szypowi.cz>
2025-08-27 10:28:38 +0200

    reuse pane if last active

--- dot-config/fish/functions/start-tmux.fish
+++ dot-config/fish/functions/start-tmux.fish
@@ -5,7 +5,18 @@ function start-tmux
         set -l tmux_session_name "vscode-$VSCODE_WORKSPACE_FOLDER_BASENAME"
 
         if tmux has-session -t $tmux_session_name 2>/dev/null
-            exec tmux attach-session -d -t $tmux_session_name \; new-window -c "$PWD"
+            # check if the pane current path is the same as $PWD
+            set -l current_pane_path (tmux list-sessions -f "#{==:#{session_name},$tmux_session_name}" -F "#{pane_current_path}")
+
+            if [ "$current_pane_path" = "$PWD" ]
+                # if yes, just attach to the session
+                exec tmux attach-session -d -t $tmux_session_name
+            else
+                # if not, split the window to open a new pane with the current path
+                # and attach to the session
+                exec tmux attach-session -d -t $tmux_session_name \; split-window -c "$PWD"
+            end
+
         else
             exec tmux new-session -s $tmux_session_name -c "$PWD"
         end
