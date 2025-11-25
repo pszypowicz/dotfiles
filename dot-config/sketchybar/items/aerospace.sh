53fa7ec420b866f906fbad7eef2f4c4b337af98e
Przemek Szypowicz <p.code@szypowi.cz>
2025-11-25 10:00:12 +0100

    native alias for aerospace

--- dot-config/sketchybar/items/aerospace.sh
+++ dot-config/sketchybar/items/aerospace.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-sketchybar --add event aerospace_focus_changed
-sketchybar --add event aerospace_workspace_change
+# sketchybar --add event aerospace_focus_changed
+# sketchybar --add event aerospace_workspace_change
 
-sketchybar --add alias "AeroSpace" left \
-  --set AeroSpace \
+sketchybar --add alias "Control Center,bobko.aerospace" left \
+  --set "Control Center,bobko.aerospace" \
   icon.drawing=off \
   label.drawing=off \
   padding_left=0 \
-  padding_right=0 \
-  --subscribe AeroSpace aerospace_workspace_change \
-  --subscribe AeroSpace aerospace_focus_changed
+  padding_right=0
+# --subscribe "Control Center,bobko.aerospace" aerospace_workspace_change \
+# --subscribe "Control Center,bobko.aerospace" aerospace_focus_changed
