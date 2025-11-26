148f2f023273e01c31f077be4e0f63668bf0181c
Przemysław Szypowicz <2733699+pszypowicz@users.noreply.github.com>
2025-11-26 14:16:23 +0100

    always redraw workspace items

--- dot-config/sketchybar/items/aerospace.sh
+++ dot-config/sketchybar/items/aerospace.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
 PLUGIN_DIR="$CONFIG_DIR/plugins"
-source "$CONFIG_DIR/colors.sh"
 
 # Register custom events
 sketchybar --add event aerospace_workspace_change
-sketchybar --add event display_added NSSecondaryDisplayVisChanged
 
 # Create hidden dummy item to listen for display changes
 sketchybar --add item aerospace.observer left \
   --set aerospace.observer \
   drawing=off \
   script="$PLUGIN_DIR/aerospace.sh" \
-  --subscribe aerospace.observer display_added \
   --subscribe aerospace.observer aerospace_workspace_change
