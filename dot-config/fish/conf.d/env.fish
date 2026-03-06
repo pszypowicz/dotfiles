275148570cf5ddaa100d96b357344063395d4672
Przemysław Szypowicz <2733699+pszypowicz@users.noreply.github.com>
2026-03-06 13:21:40 +0100

    better fzf

--- dot-config/fish/conf.d/env.fish
+++ dot-config/fish/conf.d/env.fish
@@ -5,7 +5,8 @@ set -gx FZF_DEFAULT_COMMAND 'fd --type f'
 set -gx FZF_DEFAULT_OPTS '--tmux bottom,80%,70%,border-native'
 set -gx FZF_ALT_C_COMMAND 'fd --type d'
 set -gx FZF_ALT_C_OPTS '--preview "lsd --tree --depth 1 {}"'
-set -gx FZF_CTRL_T_OPTS '--preview "bat --color=always --style=numbers {}"'
+set -gx FZF_CTRL_T_OPTS '--select-1 --exit-0 --preview "bat --color=always --style=numbers {}"'
+set -gx FZF_CTRL_R_OPTS '--preview "echo {}" --preview-window down:3:hidden:wrap --bind "?:toggle-preview"'
 
 set -gx XDG_CONFIG_HOME ~/.config
 
