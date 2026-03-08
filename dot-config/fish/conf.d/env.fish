717d710a8bbb405a1028c60345214ebb4ec64cab
Przemysław Szypowicz <2733699+pszypowicz@users.noreply.github.com>
2026-03-08 15:43:15 +0100

    Add preview
    Cleanup not used visual studio code tmux path

--- dot-config/fish/conf.d/env.fish
+++ dot-config/fish/conf.d/env.fish
@@ -3,10 +3,10 @@ set -gx EDITOR vim
 
 set -gx FZF_DEFAULT_COMMAND 'fd --type f'
 set -gx FZF_DEFAULT_OPTS '--tmux bottom,80%,70%,border-native'
-set -gx FZF_ALT_C_COMMAND 'fd --type d'
+set -gx FZF_ALT_C_COMMAND '{ zoxide query --list 2>/dev/null; fd --type d; } | awk "!seen[\$0]++"'
 set -gx FZF_ALT_C_OPTS '--preview "lsd --tree --depth 1 {}"'
-set -gx FZF_CTRL_T_OPTS '--select-1 --exit-0 --preview "bat --color=always --style=numbers {}"'
-set -gx FZF_CTRL_R_OPTS '--preview "echo {}" --preview-window down:3:hidden:wrap --bind "?:toggle-preview"'
+set -gx FZF_CTRL_T_OPTS '--select-1 --exit-0 --preview "test -d {} && lsd --tree --depth 1 {} || bat --color=always --style=numbers {}"'
+set -gx FZF_CTRL_R_OPTS '--layout=reverse --preview "echo {}" --preview-window down:3:hidden:wrap --bind "?:toggle-preview"'
 
 set -gx XDG_CONFIG_HOME ~/.config
 
--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,17 +1,18 @@
 set fish_greeting
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+/opt/homebrew/bin/brew shellenv | source
+fish_add_path ~/.local/bin
 
 if status is-interactive
     # Check if tmux is already running by checking the TMUX environment variable
     # Do not run tmux if inside VSCode terminal
-    if not set -q TMUX; and not string match -q "$TERM_PROGRAM" vscode
-        ,start-tmux
+    if not set -q TMUX; and not string match -q "$TERM_PROGRAM" vscode; and isatty stdin
+        exec tmux new-session -A -D -s default -c "$PWD"
     end
 
     starship init fish | source
     fzf --fish | source
+    zoxide init fish | source
 
     ssh-add --apple-use-keychain -q
 end
-export PATH="$HOME/.local/bin:$PATH"
