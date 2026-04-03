594e4e96e73c86bf6c2b89eaf2ff15a3da286615
Przemysław Szypowicz <2733699+pszypowicz@users.noreply.github.com>
2026-04-03 22:51:33 +0200

    Add XDG env vars to relocate dotfiles out of ~/
    Set GRADLE_USER_HOME, NPM_CONFIG_USERCONFIG, BUNDLE_USER_*,
    LESSHISTFILE, ANDROID_USER_HOME, DOTNET_CLI_HOME, HISTFILE across
    fish, zsh, and bash. Move viminfo to XDG state dir. Add npmrc to
    redirect npm cache. Disable macOS zsh_sessions.

--- dot-config/fish/conf.d/env.fish
+++ dot-config/fish/conf.d/env.fish
@@ -9,7 +9,22 @@ set -gx FZF_CTRL_T_OPTS '--select-1 --exit-0 --preview "test -d {} && lsd --tree
 set -gx FZF_CTRL_R_OPTS '--layout=reverse --preview "echo {}" --preview-window down:3:hidden:wrap --bind "?:toggle-preview"'
 
 set -gx XDG_CONFIG_HOME ~/.config
+set -gx XDG_DATA_HOME ~/.local/share
+set -gx XDG_STATE_HOME ~/.local/state
+set -gx XDG_CACHE_HOME ~/.cache
 set -gx CLAUDE_CONFIG_DIR ~/.config/claude
+
+# Move dotfiles out of ~/
+set -gx GRADLE_USER_HOME $XDG_DATA_HOME/gradle
+set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
+set -gx BUNDLE_USER_HOME $XDG_DATA_HOME/bundler
+set -gx BUNDLE_USER_CONFIG $XDG_CONFIG_HOME/bundler
+set -gx BUNDLE_USER_CACHE $XDG_CACHE_HOME/bundler
+set -gx LESSHISTFILE $XDG_STATE_HOME/less/history
+set -gx ANDROID_USER_HOME $XDG_DATA_HOME/android
+set -gx DOTNET_CLI_HOME $XDG_DATA_HOME
+set -gx HISTFILE $XDG_STATE_HOME/zsh/history
+set -gx SHELL_SESSIONS_DISABLE 1
 
 fish_add_path /Users/pszypowicz/bin
 
